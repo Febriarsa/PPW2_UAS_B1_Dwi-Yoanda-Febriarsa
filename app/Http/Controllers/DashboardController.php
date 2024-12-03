e910f4b168d011dfa9a120ac9992f25bf060a9f9
Febriarsa <dwiyoandafebriarsa@mail.ugm.ac.id>
2024-12-03 14:18:48 +0700

    perbaikan pada view dan controller dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi; // Import the model if you're fetching data from the database
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count = 0;
-        return view('dashboard', ['transaksi_count' => $transaksi_count]);
+        $transaksi_count = Transaksi::count();
+
+        $item_count = 0;
+        $omzet = 0; 
+
+        return view('dashboard', compact('transaksi_count', 'item_count', 'omzet'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts')
+
+@section('content')
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header">SUMMARY</div>
@@ -7,7 +10,9 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Transaksi</h5>
-                            <h4><span class="badge text-bg-secondary">{{ number_format($transaksi_count, 0, '.', '.') }}</span></h4>
+                            <h4>
+                                <span class="badge text-bg-secondary">{{ number_format($transaksi_count, 0, '.', '.') }}</span>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -15,19 +20,35 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Item Terjual</h5>
-                            <h4><span class="badge text-bg-secondary">{{ number_format($item_terjual_count, 0, '.', '.') }}</span></h4>
+                            <h4>
+                                <span class="badge text-bg-secondary">{{ number_format($item_count, 0, '.', '.') }}</span>
+                            </h4>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Total Pendapatan</h5>
-                            <h4><span class="badge text-bg-secondary">{{ number_format($total_pendapatan, 0, '.', '.') }}</span></h4>
+                            <h5 class="card-title">Omzet</h5>
+                            <h4>
+                                <span class="badge text-bg-secondary">Rp {{ number_format($omzet, 0, '.', '.') }}</span>
+                            </h4>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="card">
+        <div class="card-header">DATA</div>
+        <div class="card-body">
+            <div class="list-group">
+                <a href="{{ route('transaksi.create') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
+                <a href="{{ route('transaksi.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
+                <a href="{{ route('transaksidetail.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
