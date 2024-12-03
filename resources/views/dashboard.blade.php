5692346181134015fe2664da8e1a3b972190dcc6
Febriarsa <dwiyoandafebriarsa@mail.ugm.ac.id>
2024-12-03 13:59:25 +0700

    perbaikan pada dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header">SUMMARY</div>
@@ -16,29 +15,18 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Item Terjual</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4><span class="badge text-bg-secondary">{{ number_format($item_terjual_count, 0, '.', '.') }}</span></h4>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Omzet</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h5 class="card-title">Total Pendapatan</h5>
+                            <h4><span class="badge text-bg-secondary">{{ number_format($total_pendapatan, 0, '.', '.') }}</span></h4>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">DATA</div>
-        <div class="card-body">
-            <div class="list-group">
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
             </div>
         </div>
     </div>
